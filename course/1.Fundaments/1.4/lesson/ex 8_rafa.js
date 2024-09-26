93e40cc7524caa370479e931877c4ff7a76d4b84
unknown <rafaelsanches2002@yahoo.com.br>
2024-09-25 22:44:15 -0300

    ex 7

--- course/1.Fundaments/1.4/lesson/ex 8_rafa.js
+++ course/1.Fundaments/1.4/lesson/ex 8_rafa.js
@@ -83,3 +83,123 @@ if (somaAngulos > 180){
 else {
     console.log(resultado = true)
 }
+
+console.log("EXERCICIO 5")
+
+//exercicio 5
+let pecaXadrez = "CAVALO";
+pecaXadrez = pecaXadrez.toLowerCase();
+
+switch (pecaXadrez){
+    case "cavalo":
+    console.log("movimenta em L")
+    break;
+    case "bispo":
+    console.log("movimenta livre na diagonal")
+    break;
+    case "rei":
+    console.log("movimenta livre em qualquer direção apenas 1 casa por vez")
+    break;
+    case "rainha":
+    console.log("movimenta livre pelo tabuleiro")
+    break;
+    case "torre":
+    console.log("movimenta livre apenas na horizontal ou na vertical")
+    break;
+    case "peão":
+    console.log("movimenta apenas 1 uma casa para frente por vez")
+    break;  
+    default:
+    console.log("Peça não existe")
+    break; 
+}
+
+console.log("EXERCICIO 7")
+
+//exercicio 7
+
+let salarioBruto = 3000;
+let salario_INSS = 0;
+let salarioBase = 0;
+let salarioLiquido = 0;
+
+if (salarioBruto < 1556.94){
+    salario_INSS = salarioBruto - (salarioBruto*0.08);
+    salarioLiquido = salario_INSS
+    console.log("O salário líquido é de: " + salarioLiquido)
+}
+
+else if (salarioBruto > 1556.95 && salarioBruto < 2594.92){
+    salarioBase = salarioBruto - (salarioBruto*0.09);
+
+    if (salarioBase < 1903.99){
+        salarioLiquido = salarioBase;
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 1903.99 && salarioBase < 2826.65){
+        salarioLiquido = salarioBase - ((salarioBase*0.075) - 142.80);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 2826.66 && salarioBase < 3751.05){
+        salarioLiquido = salarioBase - ((salarioBase*0.15) - 354.80);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 3751.06 && salarioBase < 4664.68){
+        salarioLiquido = salarioBase - ((salarioBase*0.225) - 636.13);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else {
+        salarioLiquido = salarioBase - ((salarioBase*0.275) - 869.36);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+}
+
+else if (salarioBruto > 2594.93 && salarioBruto < 5189.82){
+    salarioBase = salarioBruto - (salarioBruto*0.11);
+
+    if (salarioBase < 1903.99){
+        salarioLiquido = salarioBase;
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 1903.99 && salarioBase < 2826.65){
+        salarioLiquido = salarioBase - ((salarioBase*0.075) - 142.80);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 2826.66 && salarioBase < 3751.05){
+        salarioLiquido = salarioBase - ((salarioBase*0.15) - 354.80);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 3751.06 && salarioBase < 4664.68){
+        salarioLiquido = salarioBase - ((salarioBase*0.225) - 636.13);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else {
+        salarioLiquido = salarioBase - ((salarioBase*0.275) - 869.36);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+}
+
+else if (salarioBruto > 5189.82){
+    salarioBase = salarioBruto - 570.88;
+
+    if (salarioBase < 1903.99){
+        salarioLiquido = salarioBase;
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 1903.99 && salarioBase < 2826.65){
+        salarioLiquido = salarioBase - ((salarioBase*0.075) - 142.80);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 2826.66 && salarioBase < 3751.05){
+        salarioLiquido = salarioBase - ((salarioBase*0.15) - 354.80);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else if (salarioBase > 3751.06 && salarioBase < 4664.68){
+        salarioLiquido = salarioBase - ((salarioBase*0.225) - 636.13);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+    else {
+        salarioLiquido = salarioBase - ((salarioBase*0.275) - 869.36);
+        console.log("O salário líquido é de: " + salarioLiquido)
+    }
+}
